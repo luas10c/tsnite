9a8df2e73f3d4288a36524b0eee86dfaed5df63b
Luciano Alves <luas10c@gmail.com>
2026-04-12 05:48:15 -0300

    test: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
-/** @type{import('jest').Config} */
-const config = {
+import { defineConfig } from 'jest'
+
+export default defineConfig({
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/cli.ts',
+    '!<rootDir>/src/register.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   testEnvironment: '<rootDir>/tests/environment.ts',
   moduleNameMapper: {
@@ -8,6 +22,4 @@ const config = {
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest'
   }
-}
-
-export default config
+})
