aed2655df9a141110e197e0850b183af12fe05aa
Luciano Alves <luas10c@gmail.com>
2026-04-12 05:48:36 -0300

    chore: update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+import globals from 'globals'
+
 import js from '@eslint/js'
 
 import ts from 'typescript-eslint'
@@ -9,24 +11,41 @@ import prettier from 'eslint-plugin-prettier'
 import jest from 'eslint-plugin-jest'
 
 export default defineConfig([
-  globalIgnores(['node_modules', 'dist', 'coverage']),
+  globalIgnores(['node_modules', 'coverage', 'dist']),
   js.configs.recommended,
   ts.configs.recommended,
   {
+    name: 'prettier/recommended',
     plugins: {
       prettier
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.node,
+        ...globals.es2024
+      },
+      sourceType: 'module'
     },
     rules: {
       ...prettier.configs.recommended.rules
     }
   },
   {
+    name: 'jest/recommended',
     files: ['**/*.{test,spec}.ts'],
     plugins: {
       jest
     },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.jest
+      },
+      sourceType: 'module'
+    },
     rules: {
-      ...jest.configs['flat/recommended']
+      ...jest.configs.recommended.rules
     }
   }
 ])
